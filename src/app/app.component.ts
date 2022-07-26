177a89b0ec8e7d4efbbe0e7429facd8a0ec4d171
Tasha L <natasha.lewis.lalor@gmail.com>
2022-07-26 00:14:14 -0500

    First draft, no bonuses. Creating branch for bonuses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,8 +42,12 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
+			let type = this.sourceList[i].type.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
+			}
+			if (type.indexOf(searchTerm) >= 0 && !matchingSatellites.includes(this.sourceList[i])) {
+				matchingSatellites.push(this.sourceList[i])
 			}
 		}
 		// assign this.displayList to be the array of matching satellites
